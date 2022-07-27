714fb9b51711789e7025ae7d69bef0b260980304
MargheritaAvolio <margheritavolio.magmail.com>
2022-07-27 13:25:31 +0200

    testSpring + annotazioniEntità

---
+++ testSpring/src/main/java/school/devskill/testSpring/model/Prodotto.java
@@ -0,0 +1,51 @@
+package school.devskill.testSpring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+
+public class Prodotto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 100)
+    private String descrizione;
+
+    @Column(nullable = false,length = 100)
+    private String immagine;
+
+    @Column(nullable = false, length = 100)
+    private String dimensione;
+
+    @Column(nullable = false, length = 100)
+    private String titolo;
+
+    @Column(nullable = false, length = 100)
+    private String categoria;
+
+    @Column(nullable = false, length = 100)
+    private String tipo;
+
+    @Column(nullable = false)
+    private int autore;
+
+    @Column(nullable = false)
+    private int nPagine;
+
+
+
+}
---
+++ testSpring/src/main/java/school/devskill/testSpring/model/Utente.java
@@ -0,0 +1,32 @@
+package school.devskill.testSpring.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class Utente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false,length = 100)
+    private String cognome;
+
+    @Column(nullable = false, length = 100)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+}
